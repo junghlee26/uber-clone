77ac27e71f0f7b4f5d0ae59c63d03b3b49ae179f
Junghoon Lee <raw.bridge@gmail.com>
2018-09-20 13:44:04 -0700

    initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+## Uber-clone
+### Require Firbase set up to run the program
---
+++ Uber/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Uber
+//
+//  Created by Junghoon Lee on 9/19/18.
+//  Copyright © 2018 Junghoon Lee. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var driverLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    var signUpMode = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func topTapped(_ sender: Any) {
+        if signUpMode {
+            //sign up process
+        } else {
+            //log in process
+        }
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        if signUpMode {
+            topButton.setTitle("Log In", for: .normal)
+            bottomButton.setTitle("Switch to Sign Up", for: .normal)
+            riderLabel.isHidden = true
+            driverLabel.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMode = false
+        } else {
+            topButton.setTitle("Sign Up", for: .normal)
+            bottomButton.setTitle("Switch to Log In", for: .normal)
+            riderLabel.isHidden = false
+            driverLabel.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMode = true
+        }
+    }
+    
+}
+
